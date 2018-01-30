1a5e2b9d0edaca059c8f4fea3169f0a8bd0af69a
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2018-01-30 12:03:39 +0200

    Created LoginActivity.java, added method stubs.

---
+++ app/src/main/java/com/example/ryhma4/taskimatti/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.ryhma4.taskimatti;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by mikae on 30.1.2018.
+ */
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    private void createAccount() {
+
+    }
+
+    private void signIn() {
+
+    }
+
+    private void signOut() {
+
+    }
+
+    private boolean validateForm() {
+        return false;
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+    }
+}
