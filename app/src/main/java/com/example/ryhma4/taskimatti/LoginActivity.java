f9d7d0c654c737b1cc5c701ce4a7b1066276d81d
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2018-01-30 12:23:29 +0200

    LoginActivity, added onclick test functionality.

--- app/src/main/java/com/example/ryhma4/taskimatti/LoginActivity.java
+++ app/src/main/java/com/example/ryhma4/taskimatti/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.ryhma4.taskimatti;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
@@ -12,20 +13,25 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
-
-    @Override
-    public void onClick(View view) {
-
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+
     }
 
     @Override
     public void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        if(viewId == R.id.btnLogin) {
+            TextView txtTest = (TextView)findViewById(R.id.txtTest);
+            txtTest.setText("Login button success!");
+        }
     }
 
     private void createAccount() {
