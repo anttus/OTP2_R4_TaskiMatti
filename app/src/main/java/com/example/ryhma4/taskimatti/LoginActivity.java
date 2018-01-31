5bd508eed3b32059d94a8c6aec05064cebac3967
Anttu Suhonen <anttu.suhonen@gmail.com>
2018-01-31 14:10:17 +0200

    Login button test log output

--- app/src/main/java/com/example/ryhma4/taskimatti/LoginActivity.java
+++ app/src/main/java/com/example/ryhma4/taskimatti/LoginActivity.java
@@ -1,40 +1,29 @@
 package com.example.ryhma4.taskimatti;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-/**
- * Created by mikae on 30.1.2018.
- */
-
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private AppCompatButton btnLogin;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
 
-        // Buttons
-        findViewById(R.id.btnLogin).setOnClickListener(this);
-        findViewById(R.id.btnSignUp).setOnClickListener(this);
-
-        btnLogin = findViewById(R.id.btnLogin);
-
-        btnLogin.setOnClickListener(new View.OnClickListener(){
+        AppCompatButton btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                Log.d("myTag", "This is my message");
+                Log.d("Test", "Testing if button is pressed");
             }
         });
 
@@ -45,15 +34,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onStart() {
         super.onStart();
-    }
-
-    @Override
-    public void onClick(View view) {
-        int viewId = view.getId();
-        if(viewId == R.id.btnLogin) {
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-        }
     }
 
     private void createAccount() {
