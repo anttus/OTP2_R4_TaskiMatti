07548048ce155b00cb2c9cc524f76d3197c97c8e
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2018-02-05 12:24:15 +0200

    Created Database and Routine classes, extremely WIP skeletons.

---
+++ app/src/main/java/com/example/ryhma4/taskimatti/Database.java
@@ -0,0 +1,27 @@
+package com.example.ryhma4.taskimatti;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by mikae on 5.2.2018.
+ */
+
+public class Database {
+    private FirebaseDatabase database;
+    private DatabaseReference mDatabase;
+
+    public Database() {
+    database = FirebaseDatabase.getInstance();
+    mDatabase = database.getReference();
+
+    }
+
+    public void setRoutine( Routine routine) {
+        mDatabase.child("routines").child(routine.getID()).setValue(routine);
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/ryhma4/taskimatti/Routine.java
@@ -0,0 +1,18 @@
+package com.example.ryhma4.taskimatti;
+
+/**
+ * Created by mikae on 5.2.2018.
+ */
+
+public class Routine {
+
+    private String ID, name, creator, description;
+
+    public Routine() {
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+}
