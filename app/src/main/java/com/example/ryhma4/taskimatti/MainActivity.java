6da63591c5cffbb762cb3e22ac80daac88b88163
Anttu Suhonen <anttu.suhonen@gmail.com>
2018-02-07 15:01:40 +0200

    AndroidManifest and MainActivity: Pressing back button while in activity_main closes the app.

--- app/src/main/java/com/example/ryhma4/taskimatti/MainActivity.java
+++ app/src/main/java/com/example/ryhma4/taskimatti/MainActivity.java
@@ -42,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
