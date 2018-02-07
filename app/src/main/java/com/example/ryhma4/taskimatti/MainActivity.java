6d91523ee9f6608c24055729bdb5d269f9d6d2cf
Anttu Suhonen <anttu.suhonen@gmail.com>
2018-02-07 14:10:37 +0200

    Cleaning up MainActivity

--- app/src/main/java/com/example/ryhma4/taskimatti/MainActivity.java
+++ app/src/main/java/com/example/ryhma4/taskimatti/MainActivity.java
@@ -43,41 +43,28 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
-//        return super.onOptionsItemSelected(item);
 
         switch (item.getItemId()) {
 
             case R.id.action_main:
                 startActivity(new Intent(this, MainActivity.class));
-                setContentView(R.layout.activity_main);
                 Toast.makeText(this, "Main", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_settings:
                 startActivity(new Intent(this, Settings.class));
-                setContentView(R.layout.activity_settings);
                 Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_setTasks:
                 startActivity(new Intent(this, SetTasks.class));
-                setContentView(R.layout.activity_set_tasks);
                 Toast.makeText(this, "Already in Set Tasks", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_routines:
                 startActivity(new Intent(this, Routines.class));
-                setContentView(R.layout.activity_routines);
                 Toast.makeText(this, "Routines", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_signOut:
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                setContentView(R.layout.login_main);
                 finish();
                 return true;
             default:
