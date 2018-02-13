b694ad26864f106c6a7d93bdc5282d9a18c6b239
Anttu Suhonen <anttu.suhonen@gmail.com>
2018-02-13 13:25:30 +0200

    Adding number of tasks adds elements to create them

--- app/src/main/java/com/example/ryhma4/taskimatti/CreateRoutineActivity.java
+++ app/src/main/java/com/example/ryhma4/taskimatti/CreateRoutineActivity.java
@@ -7,11 +7,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,17 +23,21 @@ import com.example.ryhma4.taskimatti.data.Database;
 import com.example.ryhma4.taskimatti.data.Routine;
 import com.example.ryhma4.taskimatti.data.Type;
 
-import org.w3c.dom.Text;
-
 public class CreateRoutineActivity extends MainActivity {
+
+    private TextView routineNameView;
+    private TextView routineTypeView;
+    private EditText routineIntervalNumberView;
+    private Spinner routineIntervalView;
+    private EditText routineDurationHoursView;
+    private EditText routineDurationMinutesView;
+    private TextView routineDescriptionView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_routine);
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         // List for the routine intervals
         Spinner dropdownInterval = findViewById(R.id.dropdownInterval);
@@ -38,17 +45,46 @@ public class CreateRoutineActivity extends MainActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, intervals);
         dropdownInterval.setAdapter(adapter);
 
+        routineNameView = findViewById(R.id.inputRoutineName);
+        routineTypeView = findViewById(R.id.inputRoutineType);
+        routineIntervalNumberView = findViewById(R.id.numTimes);
+        routineIntervalView = findViewById(R.id.dropdownInterval);
+        routineDurationHoursView = findViewById(R.id.inputHours);
+        routineDurationMinutesView = findViewById(R.id.inputMinutes);
+        routineDescriptionView = findViewById(R.id.inputDescription);
+
         Button btnSaveRoutine = findViewById(R.id.btnSaveRoutine);
         btnSaveRoutine.setOnClickListener(buttonListener);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        routineIntervalNumberView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Toast.makeText(CreateRoutineActivity.this, charSequence, Toast.LENGTH_SHORT).show();
+                createNewRows();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+    }
+
+    public void createNewRows() {
+        Toast.makeText(CreateRoutineActivity.this, "asdf", Toast.LENGTH_SHORT).show();
+        LinearLayout linearLayout = new LinearLayout(this);
+
+        TextView textView1 = new TextView(this);
+        textView1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        textView1.setText("programmatically created TextView1");
+        textView1.setBackgroundColor(0xff66ff66); // hex color 0xAARRGGBB
+        textView1.setPadding(20, 20, 20, 20);// in pixels (left, top, right, bottom)
+        linearLayout.addView(textView1);
     }
 
     @Override
@@ -58,13 +94,6 @@ public class CreateRoutineActivity extends MainActivity {
 
     private View.OnClickListener buttonListener = new View.OnClickListener() {
         public void onClick(View v) {
-            TextView routineNameView = findViewById(R.id.inputRoutineName);
-            TextView routineTypeView = findViewById(R.id.inputRoutineType);
-            EditText routineIntervalNumberView = findViewById(R.id.numTimes);
-            Spinner routineIntervalView = findViewById(R.id.dropdownInterval);
-            EditText routineDurationHoursView = findViewById(R.id.inputHours);
-            EditText routineDurationMinutesView = findViewById(R.id.inputMinutes);
-            TextView routineDescriptionView = findViewById(R.id.inputDescription);
 
             String routineName = routineNameView.getText().toString();
             Type routineType = new Type(routineTypeView.getText().toString(), "#FFFFFF");
@@ -82,5 +111,6 @@ public class CreateRoutineActivity extends MainActivity {
 
         }
     };
+
 
 }
