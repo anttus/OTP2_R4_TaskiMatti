942f345c69684392310a8b4e4bcdea4b24bfa980
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2018-02-28 16:20:50 +0200

    ShowRoutineActivity, current state of WIP

--- app/src/main/java/com/example/ryhma4/taskimatti/activity/ShowRoutinesActivity.java
+++ app/src/main/java/com/example/ryhma4/taskimatti/activity/ShowRoutinesActivity.java
@@ -19,10 +19,11 @@ import java.util.List;
 public class ShowRoutinesActivity extends MainActivity implements CallbackHandler {
     private ExpandableListView listView, listViewAllRoutines;
     private ExpandableListAdapter listAdapter;
-    private List<String> listDataHeader, listAllRoutines;
+    private List<String> listDataHeader, listAllRoutines, allRoutines;
     private HashMap<String, List<String>> listHashMap;
     private Database db;
     private ArrayList<List> listOfTypes;
+    private ArrayList<ArrayList<String>> routinesByType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,34 +39,51 @@ public class ShowRoutinesActivity extends MainActivity implements CallbackHandle
         listDataHeader = new ArrayList<>();
         listAllRoutines = new ArrayList<>();
         listHashMap = new HashMap<>();
+        routinesByType = new ArrayList<>();
+        routinesByType.add(new ArrayList<String>());
+        allRoutines = new ArrayList<>();
     }
 
-    private void initData(Routine routine, ArrayList listOfTypes) {
+    private void initData(Routine routine) {
         listDataHeader.clear();
         listAllRoutines.clear();
 
         listAllRoutines.add("KAIKKI RUTIINIT");
 
-        List<List<String>> allRoutines = new ArrayList<List<String>>();
+//        List<List<String>> allRoutines = new ArrayList<List<String>>();
 
-        ArrayList<String> temp = new ArrayList<>();
+//        ArrayList<String> temp = new ArrayList<>();
 
-        for (int i = 0; i < allRoutines.size(); i++){
-            for(int j = 0; j < allRoutines.get(i).size(); j++){
-                temp.add(allRoutines.get(i).get(j));
-            }
-        }
-
-        if (listDataHeader.indexOf(routine.getType().getName()) < 0) {
-            listDataHeader.add(routine.getType().getName());
-        }
-
+//        for (int i = 0; i < allRoutines.size(); i++){
+//            for(int j = 0; j < allRoutines.get(i).size(); j++){
+//                temp.add(allRoutines.get(i).get(j));
+//            }
+//        }
         int index = listDataHeader.indexOf(routine.getType().getName());
+
+        if (index < 0) {
+            listDataHeader.add(routine.getType().getName());
+            index = listDataHeader.indexOf(routine.getType().getName());
+        }
+
+        if(routinesByType.get(index) == null) {
+            routinesByType.add(new ArrayList<String>());
+        }
+
+        allRoutines.add(routine.getName());
+        routinesByType.get(index).add(routine.getName());
+        Log.w("HEADER",listDataHeader.get(index));
+        Log.w("LIST", routinesByType.get(index).get(0));
 
 //        listAllRoutines.get(0).add(routine.getName());
 
 
-        listHashMap.put(listAllRoutines.get(0), temp);
+        listHashMap.put(listAllRoutines.get(0), allRoutines);
+        Log.w("ALLROUTINES", allRoutines.toString());
+
+        for (int i = 0; i < listDataHeader.size(); i++) {
+            listHashMap.put(listDataHeader.get(i), routinesByType.get(i));
+        }
         listAdapter = new ExapandableListAdapter(this,listAllRoutines,listHashMap);
         listViewAllRoutines.setAdapter(listAdapter);
     }
@@ -91,11 +109,11 @@ public class ShowRoutinesActivity extends MainActivity implements CallbackHandle
 
     @Override
     public void passRoutine(Routine routine) {
-        listOfTypes.add(new ArrayList());
+//        listOfTypes.add(new ArrayList());
 
-        initData(routine, listOfTypes);
+        initData(routine);
 
-//        Log.w("PASS_ROUTINE", routine.getType().getName());
+        Log.w("PASS_ROUTINE", routine.getType().getName());
     }
 
 }
