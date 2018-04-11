58da2bf547bc5ec3baf6f0a762c8021e1dc34697
Anttu Suhonen <anttu.suhonen@gmail.com>
2018-04-11 12:51:06 +0300

    Somewhat working version: Tasks now show up in the calendar

--- app/src/main/java/com/example/ryhma4/taskimatti/activity/SetTaskActivity.java
+++ app/src/main/java/com/example/ryhma4/taskimatti/activity/SetTaskActivity.java
@@ -1,15 +1,9 @@
 package com.example.ryhma4.taskimatti.activity;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.support.annotation.RequiresApi;
-
-import com.example.ryhma4.taskimatti.Controller.Database;
 import com.example.ryhma4.taskimatti.R;
 import com.example.ryhma4.taskimatti.calendar.EventLister;
 import com.example.ryhma4.taskimatti.calendar.WeekViewEvent;
 import com.example.ryhma4.taskimatti.model.Task;
-import com.example.ryhma4.taskimatti.utility.CallbackHandler;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,21 +17,19 @@ import java.util.Locale;
  * Created by kurki on 7.3.2018.
  */
 
-public class SetTaskActivity extends SetTaskAbstract implements CallbackHandler {
+public class SetTaskActivity extends SetTaskAbstract {
 
-    private List<WeekViewEvent> events;
-    private Calendar startTime, endTime;
+    private static int id = 1;
 
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
         // Populate the week view with some events.
-        events = new ArrayList<>();
+        List<WeekViewEvent> events = new ArrayList<>();
         EventLister eventLister = EventLister.getInstance();
         List<Task> tasks = eventLister.getTasks();
 
-//        Database db = Database.getInstance();
-//        populateEventList(db);
-        int id = 1;
+        Calendar startTime;
+        Calendar endTime;
         for(Task task : tasks) {
             startTime = Calendar.getInstance();
 
@@ -58,60 +50,17 @@ public class SetTaskActivity extends SetTaskAbstract implements CallbackHandler 
             startTime.set(Calendar.MONTH, newMonth - 1);
             startTime.set(Calendar.YEAR, newYear);
             endTime = (Calendar) startTime.clone();
-            endTime.add(Calendar.HOUR, 1);
-            endTime.add(Calendar.MINUTE, 30);
+            endTime.add(Calendar.HOUR, task.getHours());
+            endTime.add(Calendar.MINUTE, task.getMinutes());
             endTime.set(Calendar.MONTH, newMonth - 1);
             WeekViewEvent event = new WeekViewEvent(id++, task.getName(), startTime, endTime);
-            System.out.println("HOURS: " + task.getHours() + " MINUTES: " + task.getMinutes());
             event.setColor(getResources().getColor(R.color.event_color_02));
-//            System.out.println("START: " + event.getStartTime() + " || END: " + event.getEndTime());
             events.add(event);
         }
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 3);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR, 1);
-        endTime.set(Calendar.MONTH, newMonth-1);
-        WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_01));
-//        System.out.println("EXAMPLE START: " + event.getStartTime() + "EXAMPLE END: " + event.getEndTime());
-        events.add(event);
 
         eventLister.clearList();
         return events;
     }
 
-    public void addToEventList(Task task) {
-    }
-
-    public void populateEventList(Database db) {
-        db.getSetTasks(SetTaskActivity.this);
-    }
-
-    @Override
-    public void successHandler(ArrayList<?> list) {}
-
-    @Override
-    public void errorHandler() {}
-
-    @Override
-    public void passObject(Object object) {
-        addToEventList((Task) object);
-    }
-
-    /*@Override
-    public double toWeekViewPeriodIndex(Calendar instance) {
-        return 0;
-    }
-
-    @Override
-    public List<? extends WeekViewEvent> onLoad(int periodIndex) {
-       return null;
-    }
-    */
 }
 
